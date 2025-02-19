a9c7f1cce8129cc7dded7e94d4dbf5b45d9efd16
“Kruthikmanubolu” <kruthikmanubolu@gmail.com>
2025-02-18 19:34:58 -0800

    New Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import "animate.css/animate.compat.css"
+import "animate.css/animate.compat.css";
 import ScrollAnimation from 'react-animate-on-scroll';
+import { Link } from 'react-scroll';
+
 const Navbar = () => {
   return (
     <section id='nav'>
-    <ScrollAnimation animateIn='fadeIn'>
-    <nav className="bg-gray-800 text-white py-4 ">
-      <div className="container mx-auto flex justify-between items-center">
-        <ul className="flex gap-x-12">
-          <li><a href="#home" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Home</a></li>
-          <li><a href="#about" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">About</a></li>
-          <li><a href="#projects" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Projects</a></li>
-          <li><a href="#Education" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Academics</a></li>
-          <li><a href="#experience" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Experience</a></li>
-          <li><a href="#skills" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Skills</a></li>
-          <li><a href="#contact" smooth = {true} duration = {500} offset = {-50} className="hover:text-blue-400">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
-    </ScrollAnimation>
+      <ScrollAnimation animateIn='fadeIn'>
+        <nav className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <ul className="flex gap-x-12">
+              <li><Link to="home" smooth={true} duration={500} offset={-100} className="hover:text-blue-400 cursor-pointer">Home</Link></li>
+              <li><Link to="about" smooth={true} duration={500} offset={-50} className="hover:text-blue-400 cursor-pointer">About</Link></li>
+              <li><Link to="projects" smooth={true} duration={500} offset={-10} className="hover:text-blue-400 cursor-pointer">Projects</Link></li>
+              <li><Link to="Education" smooth={true} duration={500} offset={-200} className="hover:text-blue-400 cursor-pointer">Academics</Link></li>
+              <li><Link to="experience" smooth={true} duration={500} offset={-100} className="hover:text-blue-400 cursor-pointer">Experience</Link></li>
+              <li><Link to="skills" smooth={true} duration={500} offset={-80} className="hover:text-blue-400 cursor-pointer">Skills</Link></li>
+              <li><Link to="contact" smooth={true} duration={500} offset={-120} className="hover:text-blue-400 cursor-pointer">Contact</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </ScrollAnimation>
     </section>
   );
 };
